5a25636e90a7bdbcb6687a289de7eb5e70e016b2
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-19 00:35:58 -0300

    Subindo repositorio e adicionando darkmode

---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="app-top-bar">
+    <v-app-bar app color="green lighten-1" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Campeonato Brasileiro 2020 ⚽</v-toolbar-title>
+      <v-tooltip v-if="!$vuetify.theme.dark" bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-on="on"
+            v-bind="attrs"
+            color="transparent"
+            small
+            fab
+            @click="darkMode"
+          >
+            <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
+          </v-btn>
+        </template>
+        <span>Dark Mode Off</span>
+      </v-tooltip>
+
+      <v-tooltip v-else bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-on="on"
+            v-bind="attrs"
+            color="transparent"
+            small
+            fab
+            @click="darkMode"
+          >
+            <v-icon>mdi-white-balance-sunny</v-icon>
+          </v-btn>
+        </template>
+        <span>Dark Mode On</span>
+      </v-tooltip>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="green--text text--lighten-2"
+        >
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Inicio</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+          <router-link to="/classificacao">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-soccer</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Classificação</v-list-item-title>
+            </v-list-item>
+          </router-link>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppTopBar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+  methods: {
+    darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.app-top-bar a {
+  text-decoration: none;
+}
+</style>
